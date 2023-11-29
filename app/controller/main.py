cdc0a58f9e345de65b795df36463f531e0c40b29
wz333 <wz333@cornell.edu>
2023-11-29 18:43:59 -0500

    Add camera stream

--- app/controller/main.py
+++ app/controller/main.py
@@ -1,7 +1,17 @@
 import random
+from importlib import import_module
 from flask import (
-    Blueprint, render_template, jsonify
+    Blueprint, render_template, jsonify, Response
 )
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera_pi import Camera
 
 bp = Blueprint('main', __name__)
 
@@ -10,11 +20,18 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('main/index.html',
                            title='BotanicPet')
+                        
 
 @bp.route('/camera')
 def camera():
     return render_template('main/camera.html',
                            title='BotanicPet')
+def gen(camera):
+    """Video streaming generator function."""
+    yield b'--frame\r\n'
+    while True:
+        frame = camera.get_frame()
+        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
 
 @bp.route('/chat')
 def chat():
@@ -29,3 +46,10 @@ def pet_status():
 
     return jsonify({'data':status_code})
     # return status_code
+
+@bp.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
