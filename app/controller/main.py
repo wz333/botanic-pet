b50d0460dff5ce76f8b7897193fb79e1928789ab
wz333 <wz333@cornell.edu>
2023-12-02 17:42:57 -0500

    ui update

--- app/controller/main.py
+++ app/controller/main.py
@@ -12,8 +12,32 @@ if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
 else:
     from camera_pi import Camera
+    
+# import plant monitor
+from plant_monitor import PlantMonitor
+pm = PlantMonitor()
+# import light sensor
+import time
+import board
+import adafruit_tsl2591
+
+# Create sensor object, communicating over the board's default I2C bus
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+# Initialize the sensor.
+sensor = adafruit_tsl2591.TSL2591(i2c)
+
+# plant data
+import openpyxl
+
+# Load the Excel workbook
+excel_file = '/home/pi/botanic-pet/app/controller/final_plant_data.xlsx'
+workbook = openpyxl.load_workbook(excel_file)
+sheet = workbook.active
 
 bp = Blueprint('main', __name__)
+
 
 
 @bp.route('/')
@@ -40,11 +64,24 @@ def chat():
 
 @bp.route('/pet_status')
 def pet_status():
+    row_index=3
     status_code=None
+    msg="I'm doing great!"
     status_code=random.choice(['00', '03', '03','03','04'])
-    data="{{url_for('static', filename='images/emojis/{}.gif')}}".format(status_code)
-
-    return jsonify({'data':status_code})
+    wetness = pm.get_wetness()
+    temp_c = pm.get_temp()
+    humidity = int(pm.get_humidity())
+    light = int(sensor.lux)
+    
+    # light conditions
+    if light<int(sheet.cell(row=row_index, column=8).value):
+        msg="Zzz"
+        status_code="512"
+    elif light>int(sheet.cell(row=row_index, column=9).value):
+        msg="It's dazzlingly bright!"
+        status_code="20"
+    
+    return jsonify({'data':status_code, 'wet': str(wetness), 'temp': str(temp_c), 'humi': str(humidity), 'lux': str(light), 'msg': msg})
     # return status_code
 
 @bp.route('/video_feed')
@@ -52,4 +89,7 @@ def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Close the workbook
+# workbook.close()
 
--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import subprocess
 import threading
 import queue
+import time
 
 def run_command(command, output_queue):
     """Run a command and put its output into a queue."""
@@ -27,13 +28,14 @@ def print_output(output_queue):
 output_queue = queue.Queue()
 
 # Define commands
-command1 = "make run"
-command2 = "ngrok http 15316"
+command2 = "make run"
+command1 = "ngrok http 15316"
 
 # Start the command threads
 thread1 = threading.Thread(target=run_command, args=(command1, output_queue))
 thread2 = threading.Thread(target=run_command, args=(command2, output_queue))
 thread1.start()
+time.sleep(2)
 thread2.start()
 
 # Start a thread to print output
@@ -43,6 +45,8 @@ print_thread.start()
 # Wait for the command threads to finish (you might want to add a timeout here)
 thread1.join()
 thread2.join()
+time.sleep(2)
+
 
 # Signal the print thread to stop
 output_queue.put((None, None))
