dfe810cc4e459848c602d8bccb06bbd0ac5aba70
wz333 <yuriezhao@outlook.com>
2023-12-02 15:22:39 -0500

    start script fix

--- start.py
+++ start.py
@@ -1,27 +1,49 @@
 import subprocess
 import threading
+import queue
 
-# Define the command line commands to be executed
+def run_command(command, output_queue):
+    """Run a command and put its output into a queue."""
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Continuously read output from the process
+    for line in iter(process.stdout.readline, ''):
+        output_queue.put((command, line))
+
+    # After the command finishes, check for any errors
+    stderr = process.stderr.read()
+    if stderr:
+        output_queue.put((command, f"ERROR: {stderr}"))
+
+# Function to print output from the queue
+def print_output(output_queue):
+    while True:
+        command, line = output_queue.get()
+        if line is None:  # Sentinel value to break the loop
+            break
+        print(f"Output from {command}: {line}", end='')
+
+# Create a queue to share output between threads
+output_queue = queue.Queue()
+
+# Define commands
 command1 = "make run"
 command2 = "ngrok http 15316"
 
-# Function to run a command and print its output
-def run_command(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    if process.returncode == 0:
-        print(f"Output of '{command}': {stdout.decode().strip()}")
-    else:
-        print(f"Error in '{command}': {stderr.decode().strip()}")
-
-# Create threads for each command
-thread1 = threading.Thread(target=run_command, args=(command1,))
-thread2 = threading.Thread(target=run_command, args=(command2,))
-
-# Start the threads
+# Start the command threads
+thread1 = threading.Thread(target=run_command, args=(command1, output_queue))
+thread2 = threading.Thread(target=run_command, args=(command2, output_queue))
 thread1.start()
 thread2.start()
 
-# Wait for both threads to finish
+# Start a thread to print output
+print_thread = threading.Thread(target=print_output, args=(output_queue,))
+print_thread.start()
+
+# Wait for the command threads to finish (you might want to add a timeout here)
 thread1.join()
 thread2.join()
+
+# Signal the print thread to stop
+output_queue.put((None, None))
+print_thread.join()
