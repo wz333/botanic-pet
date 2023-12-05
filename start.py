f5403b0adf818a93c3053e9a165635654e93905b
wz333 <wz333@cornell.edu>
2023-12-04 20:32:18 -0500

    start script

--- start.py
+++ start.py
@@ -5,15 +5,17 @@ import ngrok
 import time
 import pygame  # Import pygame graphics library
 import os  # for OS calls
+import sys
 import RPi.GPIO as GPIO
 import time
 import subprocess
 from pygame.locals import * #for event MOUSE variables
 
-time_lim = 300
+
+time_lim = 120
 start_time=time.time()
 
-run_flag=False
+run_flag=True
 global x,y
 x,y=None,None
 
@@ -39,14 +41,21 @@ WHITE=255,255,255
 BLACK=0,0,0
 RED = 255,0,0
 GREEN = 0,255,0
+quit_size=20
+stop_center=160,120
+stop_radius=40
 screen=pygame.display.set_mode((320,240))
 
 my_font=pygame.font.Font(None,25)
+my_font2=pygame.font.Font(None,20)
+my_font2.set_bold(False)
 my_buttons={'start':(160,120), 'quit':(300,220), 'stop':(160,120)}
 
 
 start_flag=False
-while not start_flag:
+
+
+while not start_flag and run_flag and time.time()-start_time<time_lim:
     time.sleep(0.01)
     for event in pygame.event.get():
         if(event.type is MOUSEBUTTONDOWN):
@@ -59,6 +68,7 @@ while not start_flag:
                 start_flag=True
     if not start_flag:
         screen.fill(BLACK)
+        pygame.draw.circle(screen,GREEN,stop_center,stop_radius)
         text="Start"
         text_surface=my_font.render(text, True,WHITE)
         rect=text_surface.get_rect(center=(160, 120))
@@ -76,58 +86,47 @@ listener = ngrok.connect("localhost:15316")
 
 print(f"Ingress established at: {listener.url()}");
 
-def run_command(command, output_queue):
-    """Run a command and put its output into a queue."""
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-    # Continuously read output from the process
-    for line in iter(process.stdout.readline, ''):
-        output_queue.put((command, line))
 
-    # After the command finishes, check for any errors
-    stderr = process.stderr.read()
-    if stderr:
-        output_queue.put((command, f"ERROR: {stderr}"))
 
-# Function to print output from the queue
-def print_output(output_queue):
-    while True:
-        command, line = output_queue.get()
-        if line is None:  # Sentinel value to break the loop
-            break
-        print(f"Output from {command}: {line}", end='')
 
-# Create a queue to share output between threads
-output_queue = queue.Queue()
+class ServerThread(threading.Thread):
+    def __init__(self, command):
+        super().__init__()
+        self.command = command
+        self.process = None
 
-# Define commands
-command1 = "make run"
-# command2 = "ngrok http 15316"
+    def run(self):
+        self.process = subprocess.Popen(self.command, shell=True)
+        self.process.communicate()
 
-# Start the command threads
-thread1 = threading.Thread(target=run_command, args=(command1, output_queue))
-# thread2 = threading.Thread(target=run_command, args=(command2, output_queue))
-thread1.start()
-# thread2.start()
+    def stop(self):
+        if self.process:
+            self.process.terminate()
+            self.process.wait()
 
-# # Start a thread to print output
-# print_thread = threading.Thread(target=print_output, args=(output_queue,))
-# print_thread.start()
+command = "make run" 
+server_thread = ServerThread(command)
+server_thread.start()
 
-# Wait for the command threads to finish 
-thread1.join()
 
-while run_flag:
+
+
+while run_flag and time.time()-start_time<time_lim:
     # draw STOP btn
     pygame.draw.circle(screen,RED,stop_center,stop_radius)
     text="STOP"
     text_surface=my_font.render(text, True,WHITE)
     rect=text_surface.get_rect(center=(160, 120))
     screen.blit(text_surface, rect)
-    text2=f"Ingress established at: {listener.url()}"
-    text2_surface=my_font.render(text, True,WHITE)
-    rect2=text_surface.get_rect(center=(160, 120))
+    text2="Server established at:"
+    text2_surface=my_font2.render(text2, True,WHITE)
+    rect2=text2_surface.get_rect(center=(160, 180))
     screen.blit(text2_surface, rect2)
+    text3=f"{listener.url()}"
+    text3_surface=my_font2.render(text3, True,WHITE)
+    rect3=text3_surface.get_rect(center=(160, 200))
+    screen.blit(text3_surface, rect3)
     for event in pygame.event.get():
         if(event.type is MOUSEBUTTONDOWN):
             pos=pygame.mouse.get_pos()
@@ -137,4 +136,12 @@ while run_flag:
             x,y=pos
             if my_buttons['start'][0]-quit_size<=x<=my_buttons['start'][0]+quit_size and my_buttons['start'][1]-quit_size<=y<=my_buttons['start'][1]+quit_size:
                 run_flag=False
+    pygame.display.flip()
+
+# Stop the server
+server_thread.stop()
+
+# Wait for the command threads to finish 
+pygame.quit()
+
 
